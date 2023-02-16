32daeaea67740fd2b6c9b5c2320bcf6ba3df0297
Emmanuel <elopez.mca21@lania.edu.mx>
2023-02-15 22:44:52 -0600

    initial commit

---
+++ mx/src/main/java/com/lania/mx/ArquitecturaTlmecdApplication.java
@@ -0,0 +1,13 @@
+package com.lania.mx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquitecturaTlmecdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquitecturaTlmecdApplication.class, args);
+	}
+
+}
